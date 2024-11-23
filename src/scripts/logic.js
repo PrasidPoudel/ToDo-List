9661dea3a58ecd42d2762dc38ed3ecfca8ac5be8
PrasidPoudel <prasidpoudel7.com>
2024-11-23 18:37:29 +0545

    NO duplicates allowed in project list

--- src/scripts/logic.js
+++ src/scripts/logic.js
@@ -1,19 +1,33 @@
 //This array is for all the projects and its details
-export const ProjectsArray=[]
+export const ProjectsArray = []
 //This class is to create a project by given name with user
 export class Projects {
-    constructor(project) {
-        this.project=project
+    constructor(uniqueProject) {
+        this.uniqueProject = uniqueProject
     }
 }
 
 //This class is to create project details
 export class ProjectsDetails {
-    constructor(checkbox,title,description,duedate,priority) {
-        this.checkbox=checkbox
-        this.title=title
-        this.description=description
-        this.duedate=duedate
-        this.priority=priority
+    constructor(checkbox, title, description, duedate, priority) {
+        this.checkbox = checkbox
+        this.title = title
+        this.description = description
+        this.duedate = duedate
+        this.priority = priority
     }
 }
+export function NoDuplicates(ProjectsArray, element) {
+   for(let item of ProjectsArray) {
+    if(item.uniqueProject===element.value) {
+        alert("NO Duplicate Projects Allowed");
+        return false;
+    }
+   }
+   return true;
+}
+
+
+/**
+ * WE have to check duplicate names for projects first to make it more organized
+ */
